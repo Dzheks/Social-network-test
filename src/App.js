0d3b87ccfffc37e42bfe7d277cc28e367aabfce9
Evgeniy <99970019+dzhekson1@users.noreply.github.com>
2022-05-27 15:55:12 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,9 +17,13 @@ const App = (props) => {
       <Navbar />
       <div className='app-wrapper-content'>
       <Routes>
-				<Route path="/profile" element = {<Profile />}/>
+      <Route path="/profile" element = {<Profile />}/>
 				<Route path="/dialogs/*" element = {<Dialogs />}/>
         <Route path="/news" element = {<News />}/>
+{/* 
+				<Route path="/profile" element = { () => <Profile />}/>
+				<Route path="/dialogs/*" element = {() =><Dialogs />}/>
+        <Route path="/news" element = { () => <News />}/> */}
 			</Routes>
       </div>
     </div>
